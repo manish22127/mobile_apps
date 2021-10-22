a4ca246d93cc35074cc9507f64fdf97d0c45b651
manish22127 <manish.22127@gmail.com>
2021-10-22 22:17:32 +0530

    initial

---
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.bid1);
+        txt=findViewById(R.id.t1);
+        String str=txt.getText().toString();
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("message",str);
+                startActivity(i);
+            }
+        });
+
+    }
+}
