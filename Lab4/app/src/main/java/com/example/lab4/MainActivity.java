694812ed97b6ac9eb2881d11a7688e80552723ce
manish22127 <manish.22127@gmail.com>
2021-11-17 10:01:01 +0530

    lab 4 done

---
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void alert(View view){
+        AlertDialog.Builder ab=new AlertDialog.Builder(this);
+        ab.setTitle("sure to cancel");
+        ab.setMessage("pakka");
+        ab.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"clicked yes",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ab.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"clicked no",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ab.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"clicked cancel",Toast.LENGTH_LONG).show();
+            }
+        });
+        ab.create();
+        ab.show();
+
+    }
+}
