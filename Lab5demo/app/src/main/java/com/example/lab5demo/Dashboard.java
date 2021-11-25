51bf61e8ad5bbdfda69f4660c7419c116b8d966e
manish22127 <manish.22127@gmail.com>
2021-11-25 12:11:43 +0530

    workshop

---
+++ Lab5demo/app/src/main/java/com/example/lab5demo/Dashboard.java
@@ -0,0 +1,36 @@
+package com.example.lab5demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class Dashboard extends AppCompatActivity {
+
+    SeekBar s1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        s1=findViewById(R.id.seekBar);
+
+        s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int p=progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(),"progress "+p,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
