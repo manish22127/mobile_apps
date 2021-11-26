fed4ed24831936d2d610e98d64e9275119902d2e
manish22127 <manish.22127@gmail.com>
2021-11-26 19:55:37 +0530

    "workshop MAD"

--- workshop/app/src/main/java/com/example/splash/SecondActivity.java
+++ workshop/app/src/main/java/com/example/splash/SecondActivity.java
@@ -2,13 +2,38 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class SecondActivity extends AppCompatActivity {
+    EditText ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Calendar cal=Calendar.getInstance();
+
+        ed=findViewById(R.id.ed);
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int min=cal.get(Calendar.MINUTE);
+
+        ed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog td = new TimePickerDialog(SecondActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        ed.setText(hourOfDay + ":" + minute);
+                    }
+                },hour,min,false);
+                td.show();
+            }
+        });
     }
 }
---
+++ workshop/back/app/src/main/java/com/example/back4app/Second.java
@@ -0,0 +1,17 @@
+package com.example.back4app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+    }
+}
